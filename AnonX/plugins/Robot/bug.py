0261ca3df2c19467796afb565713bd4d30c33431
Papasri <110163251+Papasri@users.noreply.github.com>
2022-09-18 20:41:26 +0530

    Update bug.py

--- AnonX/plugins/Robot/bug.py
+++ AnonX/plugins/Robot/bug.py
@@ -42,7 +42,7 @@ async def bug(_, msg: Message):
     datetimes_fmt = "%d-%m-%Y"
     datetimes = datetime.utcnow().strftime(datetimes_fmt)
 
-    thumb = "https://telegra.ph/file/56d1760224589ee370186.jpg"
+    thumb = "https://telegra.ph/file/ffb132b0a0c4dd05fa10b.jpg"
     
     bug_report = f"""
 **#ʙᴜɢ :** **@anonymous_was_bot**
