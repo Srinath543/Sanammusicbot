574fd605b0dcbf136faab8dba3c8bd5660db36fc
SRINATH <84714570+Srinath543@users.noreply.github.com>
2022-12-27 15:20:40 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -10,7 +10,7 @@ load_dotenv()
 API_ID = int(getenv("API_ID", "4508453"))
 API_HASH = getenv("API_HASH", "9fc008a9ff9656f124fb1a7fd008d68d")
 
-BOT_TOKEN = getenv("BOT_TOKEN", "1969642651:AAETxnchH3tG5qFTFI3fAVB897ZqEHj4qgA")
+BOT_TOKEN = getenv("BOT_TOKEN", "1969642651:AAEn5kHXl4sg1YrXVQhaHly3cbX86vL9ekI")
 
 MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://Sriizzy:Sriizzy@cluster0.ubi9r5h.mongodb.net/?retryWrites=true&w=majority")
 
